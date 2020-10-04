75ecfad25aa5e2ebb6dc7a53c30e2afb561e133e
teemunasanen <58671956+teemunasanen@users.noreply.github.com>
2020-10-05 01:47:04 +0300

    edit

--- Lämpötilamuunnin.java
+++ Lämpötilamuunnin.java
@@ -13,51 +13,47 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Lämpötilamuunnin implements ActionListener {
-	
+
 	JFrame frame = new JFrame("Celsius Converter");
 	JLabel label1 = new JLabel("Celsius");
 	JTextField input = new JTextField();
 	JButton button1 = new JButton("Convert");
 	JLabel output = new JLabel("Fahrenheit: ");
 
-	
-	public Lämpötilamuunnin(){
-		
+	public Lämpötilamuunnin() {
+
 		JPanel p1 = new JPanel();
 		JFrame frame = new JFrame("Celsius Converter");
-		p1.setLayout(new GridLayout (2,2));
-		
-	
-		
+		p1.setLayout(new GridLayout(2, 2));
+
 		p1.add(label1);
 		p1.add(input);
 		p1.add(button1);
 		p1.add(output);
-		
+
 		frame.add(p1);
 		p1.setBorder(BorderFactory.createLineBorder(Color.black));
-	
-		
+
 		button1.addActionListener(this);
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(450,150);
+		frame.setSize(450, 150);
 		frame.setVisible(true);
-		}
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == button1) {
-		if(isDouble(input.getText())) {
-			double numbers = Double.parseDouble(input.getText());
-			double converted = (numbers*1.8 + 32.0);
-			output.setText("Fahrenheit: " + converted);
-			
+			if (isDouble(input.getText())) {
+				double numbers = Double.parseDouble(input.getText());
+				double converted = (numbers * 1.8 + 32.0);
+				output.setText("Fahrenheit: " + converted);
 
-		}else {
-			output.setText("Numbers only!");
+			} else {
+				output.setText("Numbers only!");
+			}
 		}
-		}
-	}	
+	}
+
 	public boolean isDouble(String value) {
 		try {
 			Double.parseDouble(value);
@@ -66,12 +62,8 @@ public class Lämpötilamuunnin implements ActionListener {
 			return false;
 		}
 	}
-	
-		
-			
-		
-		
-		public static void main(String[] args) {
-				new Lämpötilamuunnin();
-		}
+
+	public static void main(String[] args) {
+		new Lämpötilamuunnin();
+	}
 }
